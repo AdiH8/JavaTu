cd9a71fd58b789e6a8e58325c64575c10894df11
adihadzhiev <adi.hadjiev@gmail.com>
2023-10-28 14:58:25 +0300

    added Task6(Class ElectricCar)

---
+++ PnpeLab2/src/Cars/ElectricCar.java
@@ -0,0 +1,28 @@
+package Cars;
+
+public class ElectricCar extends Car{
+    private double batteryLevel;
+    private double efficiency;
+
+    public ElectricCar(String brand, int speed,int mileage, double batteryLevel, double efficiency){
+        super(brand,speed,0,mileage);
+        this.batteryLevel = batteryLevel;
+        this.efficiency = efficiency;
+    }
+
+    public void charge() {
+        batteryLevel = 100;
+    }
+
+    public void driveElectric(int distance) {
+       batteryLevel-=efficiency * ((double)distance/100);
+
+    }
+    public double getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public double getEfficiency() {
+        return efficiency;
+    }
+}
