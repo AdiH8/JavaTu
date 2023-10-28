553730daba186838d1141e9fb9a458003f98743a
adihadzhiev <adi.hadjiev@gmail.com>
2023-10-28 13:33:16 +0300

    added Task1(Class Person)

---
+++ PnpeLab2/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person("John", 25);
+        person1.introduce();
+
+        Person person2 = new Person("Jane", 30);
+        person2.introduce();
+    }
+}
